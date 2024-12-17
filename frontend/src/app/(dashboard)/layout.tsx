927d315715f5ee848ba3e3b6fa971e0478820f7f
takaraktruong <truong.ta@northeastern.edu>
2024-12-16 22:07:30 -0800

    basic layout update

--- frontend/src/app/(dashboard)/layout.tsx
+++ frontend/src/app/(dashboard)/layout.tsx
@@ -3,5 +3,12 @@ export default function DashboardLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    return <div>dashboard{children}</div>;
+    return (
+        <div className="h-screen flex">
+            {/* left */}
+            <div className="w-1/6">l</div>
+            {/* right */}
+            <div className="w-5/6">r</div>
+        </div>
+    );
 }
