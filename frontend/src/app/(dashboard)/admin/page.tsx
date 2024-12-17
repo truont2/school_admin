785b9e5d3bcf26f9b3210c3c3e9bababbf3a4e26
takaraktruong <truong.ta@northeastern.edu>
2024-12-17 11:11:01 -0800

    created user cards and updated tailwind config

--- frontend/src/app/(dashboard)/admin/page.tsx
+++ frontend/src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,22 @@
+import UserCard from "@/components/UserCard";
+
 const AdminPage = () => {
-    return <div>AdminPage</div>;
+    return (
+        <div className="p-4 flex gap-4 md:flex-row flex-col">
+            {/* left */}
+            <div className="w-full lg:w-2/3">
+                {/* user cards */}
+                <div className="flex gap-4 justify-between flex-wrap">
+                    <UserCard type="student" />
+                    <UserCard type="teacher" />
+                    <UserCard type="parent" />
+                    <UserCard type="staff" />
+                </div>
+            </div>
+            {/* right */}
+            <div className="w-full lg:w-1/3">r</div>
+        </div>
+    );
 };
 
 export default AdminPage;
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +8,21 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        Sky: "#C3EBFA",
+        SkyLight: "#EDF9FD",
+        Purple: "#CFCEFF",
+        PurpleLight: "#F1F0FF",
+        Yellow: "#FAE27C",
+        YellowLight: "#FEFCE8",
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
