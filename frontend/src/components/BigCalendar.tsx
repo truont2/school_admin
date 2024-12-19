787a007d2e3dfacb560efbc372ae20d21695ac37
takaraktruong <truong.ta@northeastern.edu>
2024-12-18 22:27:25 -0800

    updated the calendar button saving

--- frontend/src/components/BigCalendar.tsx
+++ frontend/src/components/BigCalendar.tsx
@@ -7,22 +7,27 @@ import timeGridPlugin from "@fullcalendar/timegrid"; // Week and day view
 
 export default function BigCalendar() {
     // const calendarRef = useRef(null);
+
     return (
         <FullCalendar
             // ref={calendarRef}
             plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
             initialView="dayGridMonth" // Default view
             headerToolbar={{
-                left: "prev,next today",
+                left: "",
                 center: "title",
-                right: "dayGridMonth,timeGridWeek,timeGridDay", // View buttons
+                right: "timeGridDay,timeGridWeek,dayGridMonth", // View buttons
             }}
+            height={"98%"}
             editable={true} // Allows drag and drop
             selectable={true} // Allows selection
+            slotMinTime="08:00:00" // Start time at 8:00 AM
+            slotMaxTime="17:00:00" // End time at 5:00 PM
             events={[
                 { title: "Event 1", start: "2024-12-17" },
                 { title: "Event 2", start: "2024-12-18", end: "2025-06-14" },
             ]}
+            expandRows={true}
         />
     );
 }
